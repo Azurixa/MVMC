cff55ea0e8feea4df4f3d6e66ed88ff7a697a66a
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-02-15 18:36:43 +0100

    CSS refresh

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -46,6 +46,6 @@
 <link href='https://cdn.jsdelivr.net/npm/boxicons@1.9.1/css/boxicons.min.css' rel='stylesheet'>
 
 <!-- Styles -->
-<link href="{{ asset('css/app.css?v=0.5') }}" rel="stylesheet">
+<link href="{{ asset('css/app.css?v=0.6') }}" rel="stylesheet">
 <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 <link href="{{ asset('css/animate.min.css') }}" rel="stylesheet">
