d37ff18b31671e9b2c492e78dc711f4661f8fbd9
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-02-15 17:34:27 +0100

    Head unification + css refresh

--- resources/views/layouts/application.blade.php
+++ resources/views/layouts/application.blade.php
@@ -1,26 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{$title}} | MVMC</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/frontend.js') }}" defer></script>
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link href='https://cdn.jsdelivr.net/npm/boxicons@1.9.1/css/boxicons.min.css' rel='stylesheet'>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css?v=0.4') }}" rel="stylesheet">
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/animate.min.css') }}" rel="stylesheet">
+    @include('layouts.head')
 </head>
 <body>
 <div id="app">
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,20 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>Pan etc</title>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ asset('js/frontend.js') }}" defer></script>
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}" defer></script>
+
+<!-- Fonts -->
+<link href='https://cdn.jsdelivr.net/npm/boxicons@1.9.1/css/boxicons.min.css' rel='stylesheet'>
+
+<!-- Styles -->
+<link href="{{ asset('css/app.css?v=0.5') }}" rel="stylesheet">
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/animate.min.css') }}" rel="stylesheet">
