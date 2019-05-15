84d3c3d35ea94b5c9a6ba58d423ffc2d7b446f46
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-05-15 16:37:40 +0200

    /products/my route in API added and works

--- API/routes/products.js
+++ API/routes/products.js
@@ -7,24 +7,44 @@ const User = require('../models/User');
 // Middleware
 const isAuth = require('../middleware/isAuth');
 
+// Create new product
+router.post('/new', isAuth, (req, res) => {
+    const { name, brand, category, description, first_impressions } = req.body;
+
+    if (name && brand && category) {
+        Product.create({
+            user_id: req.user._id,
+            name,
+            brand,
+            category,
+            description,
+            first_impressions
+        }).then(product => {
+            res.json(product);
+        });
+    } else {
+        res.json({
+            err: 'All required fileds must be provided'
+        });
+    }
+});
+
 // Get user all products
 router.get('/my', isAuth, (req, res) => {
     const allProducts = [];
 
-    User.findById(req.user._id, ['categories'], {
-        sort: { categories: 1 }
-    }).then(user => {
-        user.categories.forEach(category => {
-            Product.find({ user_id: req.user._id, category }, [], {
-                sort: { brand: 1 }
-            }).then(products => {
-                allProducts.push({
-                    category,
-                    products
-                });
+    req.user.categories.forEach((category, index) => {
+        Product.find({ user_id: req.user._id, category }, [], {
+            sort: { brand: 1 }
+        }).then(products => {
+            allProducts.push({
+                category,
+                products
             });
+            if (index == req.user.categories.length - 1) {
+                res.json(allProducts);
+            }
         });
-        res.json(allProducts);
     });
 });
 
