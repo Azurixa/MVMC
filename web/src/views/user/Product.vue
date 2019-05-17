53dbf0939eea3c2d4e6d795cc847191a313c32de
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-05-17 20:48:32 +0200

    Git ignore + storage

--- web/src/views/user/Product.vue
+++ web/src/views/user/Product.vue
@@ -3,6 +3,42 @@
 		<router-link :to="'/me/p/' + this.$route.params.id + '/edit'">
 			Edit product
 		</router-link>
+		<div class="photos">
+			<div
+				class="photo"
+				v-for="index in 2"
+				@click="changeImage(index - 1)"
+			>
+				{{ index }}
+			</div>
+			<div class="photo new" @click="changeImage(2)">
+				<div class="card">
+					<div class="card-header">
+						<h2>
+							NEW
+						</h2>
+					</div>
+					<div class="card-body">
+						<p>
+							Add new photo
+						</p>
+						<div class="form-group">
+							<label for="newPhoto">Photo</label>
+							<input
+								type="file"
+								id="newPhoto"
+								class="form-control"
+								@change="handleFileUpload"
+								ref="newPhoto"
+							/>
+						</div>
+						<button @click="uploadNewPhoto" class="btn btn-primary">
+							Upload
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
 		<p>
 			{{ product }}
 		</p>
@@ -13,13 +49,34 @@
 export default {
 	data() {
 		return {
-			product: {}
+			product: {},
+			newPhoto: null
 		};
 	},
 	created() {
 		this.getProduct();
 	},
+	mounted() {
+		this.initGallery();
+	},
 	methods: {
+		handleFileUpload() {
+			this.newPhoto = this.$refs.newPhoto.files[0];
+		},
+		uploadNewPhoto() {
+			const formData = new FormData();
+			formData.append("_id", this.product._id);
+			formData.append("photo", this.newPhoto);
+			fetch("http://localhost:3001/products/photo", {
+				method: "POST",
+				headers: {
+					Authorization: this.$store.getters.token
+				},
+				body: formData
+			})
+				.then(res => res.json())
+				.then(data => console.log(data));
+		},
 		getProduct() {
 			fetch(
 				"http://localhost:3001/products/product/" +
@@ -38,7 +95,68 @@ export default {
 						this.product = data;
 					}
 				});
+		},
+		changeImage(index) {
+			const photos = document.querySelectorAll(".photo");
+			photos.forEach(photo => {
+				photo.removeAttribute("show");
+				photo.removeAttribute("less");
+			});
+			photos[index].setAttribute("show", true);
+			if (photos[index + 1]) {
+				photos[index + 1].setAttribute("less", true);
+			} else if (photos[index - 2]) {
+				photos[index - 2].setAttribute("less", true);
+			}
+			if (photos[index - 1]) {
+				photos[index - 1].setAttribute("less", true);
+			} else if (photos[index + 2]) {
+				photos[index + 2].setAttribute("less", true);
+			}
+		},
+		initGallery() {
+			const photos = document.querySelectorAll(".photo");
+			console.log(photos);
+			photos[0].setAttribute("show", true);
+			if (photos.length >= 1) {
+				photos[1].setAttribute("less", true);
+			}
+			if (photos.length >= 2) {
+				photos[2].setAttribute("less", true);
+			}
 		}
 	}
 };
 </script>
+
+<style lang="scss" scoped>
+.photos {
+	width: 100%;
+	height: 30vh;
+	display: block;
+
+	.photo {
+		height: 100%;
+		width: 0%;
+		display: inline-block;
+		overflow: hidden;
+		transition: 0.3s all;
+		transition-timing-function: cubic-bezier(0.895, 0.03, 0.685, 0.22);
+		background-color: black;
+		&[show] {
+			width: 70%;
+		}
+		&[less] {
+			width: 15%;
+		}
+		&.new {
+			overflow: hidden;
+			background-color: white;
+
+			.card {
+				height: 100%;
+			}
+		}
+	}
+}
+</style>
