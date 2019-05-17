8a1897f42a0c7ff58418e6512755f924d3b3b662
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-05-17 10:09:41 +0200

    Deleted '!'

--- API/middleware/isAuth.js
+++ API/middleware/isAuth.js
@@ -13,27 +13,18 @@ module.exports = (req, res, next) => {
             const tokenToCheck = tokenSplit[1];
             jwt.verify(tokenToCheck, jwtConfig.SECRET, (err, data) => {
                 if (err) {
-                    res.json({ err: 'Token is invalid!' });
+                    res.json({ err: 'Token is invalid' });
                 } else {
-                    User.findById(data._id, [
-                        'name',
-                        'email',
-                        'exp',
-                        'exp_next',
-                        'level',
-                        '_id',
-                        'categories',
-                        'brands'
-                    ]).then(user => {
+                    User.findById(data._id, {password: 0}).then(user => {
                         req.user = user;
                         next();
                     });
                 }
             });
         } else {
-            res.json({ err: 'Token in bad format!' });
+            res.json({ err: 'Token in bad format' });
         }
     } else {
-        res.json({ err: 'You need to provide auth token header!' });
+        res.json({ err: 'You need to provide auth token header' });
     }
 };
