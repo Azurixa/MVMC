f4b09d66520a6db4f267e94ecba7d9df29705c65
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-05-17 09:50:10 +0200

    Photo for product uploading POST /product/photo

--- API/routes/products.js
+++ API/routes/products.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const path = require('path');
 
 // Models
 const Product = require('../models/Product');
@@ -26,6 +27,50 @@ router.post('/new', isAuth, (req, res) => {
         res.json({
             err: 'All required fileds must be provided'
         });
+    }
+});
+
+router.post('/photo', isAuth, (req, res) => {
+    const id = req.body._id;
+    if (id) {
+        Product.findOne({ user_id: req.user._id, _id: id }).then(product => {
+            if (product) {
+                if (req.files) {
+                    const photo = req.files.photo;
+
+                    const allowedMimetypes = [
+                        'image/jpg',
+                        'image/jpeg',
+                        'image/png',
+                        'image/gif'
+                    ];
+
+                    if (allowedMimetypes.includes(photo.mimetype)) {
+                        const fileName = req.user._id + product._id + photo.name
+                        photo.mv(path.resolve('./') + '/storage/' + fileName);
+                        product.photos.push(
+                            {
+                                src: fileName
+                            }
+                        );
+                        product.save((err) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                            res.json({ msg: 'Successfully uploaded a photo' });
+                        });
+                    } else {
+                        res.json({ err: 'File must be a allowed image type' });
+                    }
+                } else {
+                    res.json({ err: 'You need to send photo' });
+                }
+            } else {
+                res.json({ err: 'Product with this ID does not exist' });
+            }
+        });
+    } else {
+        res.json({ err: 'You need to provide product id' });
     }
 });
 
