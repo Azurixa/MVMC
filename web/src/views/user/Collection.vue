d18037cf0b4acb4ecb0321c181f6e85af4949eb5
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-05-18 20:34:04 +0200

    Products collection +

--- web/src/views/user/Collection.vue
+++ web/src/views/user/Collection.vue
@@ -7,22 +7,39 @@
 			<div class="col-lg-12">
 				<div class="row">
 					<div
-						class="col-lg-4"
+						class="col-lg-4 p-1"
 						v-for="(category, index) in collection"
 						:key="'category_' + index"
 					>
-						<div class="card">
-							<div class="card-header">
-								<h3 class="m-0">{{ category.category }}</h3>
+						<div class="category">
+							<div
+								class="category-header"
+								onClick="this.nextElementSibling.toggleAttribute('show')"
+							>
+								<div class="row">
+									<div class="col-10">
+										<h3 class="m-0">
+											{{ category.category }}
+										</h3>
+									</div>
+									<div class="col-2">
+										<h3 class="m-0">
+											<i class="bx bx-sort"></i>
+										</h3>
+									</div>
+								</div>
 							</div>
-							<div class="card-body">
+							<div class="category-body">
 								<router-link
-									class="product d-block"
+									class="product d-flex"
 									v-for="product in category.products"
 									:key="product._id"
 									:to="'/me/p/' + product._id"
 								>
-									{{ product.name }}
+									<div class="thumbnail"></div>
+									<div class="info">
+										<p class="name">{{ product.name }}</p>
+									</div>
 								</router-link>
 							</div>
 						</div>
@@ -60,13 +77,73 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.product {
-	font-size: 1.1rem;
-	padding: 0.5rem;
-	border: 1px solid rgba($color: #000000, $alpha: 0.07);
-	margin-top: 1rem;
-	&:first-child {
-		margin-top: 0;
+.category {
+	border: 1px solid rgba(#000, 0.1);
+	border-radius: 1.5rem;
+	padding: 1rem 0;
+	background-color: #fefefe;
+	position: relative;
+
+	.category-header {
+		position: sticky;
+		top: 59px;
+		z-index: 1000;
+		background-color: #fefefe;
+		width: 100%;
+		margin-top: 3px;
+		padding: 1rem;
+		border: {
+			bottom: 1px solid rgba(#000, 0.1);
+		}
+		border-radius: 0 0 1rem 1rem;
+	}
+	.category-body {
+		height: 0;
+        opacity: 0;
+        overflow: hidden;
+        transition: 0.3s all;
+		&[show] {
+			height: 10%;
+            opacity: 1;
+		}
+
+		.product {
+			position: relative;
+			padding: 0;
+			background-color: #fff;
+			border: {
+				top: 1px solid rgba(#000, 0.1);
+				bottom: 1px solid rgba(#000, 0.1);
+			}
+			border-radius: 1rem;
+			margin-bottom: 1.2rem;
+            &:first-child{
+                margin-top: 1rem;
+            }
+			&:last-child {
+				margin-bottom: 0;
+			}
+
+			.thumbnail {
+				width: 35%;
+				//height: 100%;
+				display: inline-block;
+				background-color: red;
+				border-radius: 1rem 0 0 1rem;
+			}
+			.info {
+				width: 65%;
+				overflow: hidden;
+
+				.name {
+					font-weight: 800;
+					height: 1.2rem;
+					overflow: hidden;
+					word-wrap: break-word;
+					font-size: 1.1rem;
+				}
+			}
+		}
 	}
 }
 </style>
